507142307474b39d40938f849b18345aba31de43
agile-m-32 <rashid.shethwala@agileinfoways.com>
2018-02-12 11:26:02 +0530

    Initial Commit

---
+++ ColorAnimateDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorAnimateDemo
+//
+//  Created by agile-m-32 on 12/02/18.
+//  Copyright © 2018 agile-m-32. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
